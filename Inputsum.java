80190b1febf1168b8dd7714c073bf2443950ea69
snehamore23 <moresneha806@gmail.com>
2026-06-27 13:19:28 +0530

    changes done

--- Inputsum.java
+++ Inputsum.java
@@ -1,12 +1,20 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Inputsum {
-    public static void main(String arg[]){
-        Scanner sc=  new Scanner (System.in);
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
         int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
         int b = sc.nextInt();
-        int sum = a+b;
-        System.out.println(sum);
+
+        int sum = a + b;
+
+        System.out.println("The sum is: " + sum);
+
+        sc.close();
     }
-    
 }
