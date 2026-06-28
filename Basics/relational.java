3a2de24b76cdd45d07c710c6a867bf22b7d23740
snehamore23 <moresneha806@gmail.com>
2026-06-28 12:39:55 +0530

    done

---
+++ Basics/relational.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class relational {
+    public static void main (String args[]){
+
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("enetr first no:");
+        int a = sc.nextInt();
+
+        System.out.print("enetr second no:");
+        int b = sc.nextInt();
+
+        System.out.println("a==b is:"+ (a==b));
+        System.out.println("a!=b is:"+ (a!=b));
+        System.out.println("a>b is:"+ (a>b));
+        System.out.println("a<b is:"+ (a<b));
+        System.out.println("a>=b is:"+ (a>=b));
+        System.out.println("a<=b is:"+ (a<=b));
+
+        sc.close();
+                                                    
+
+    }
+    
+}
