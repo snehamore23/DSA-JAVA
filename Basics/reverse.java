1156925d9fb65958a9314db2d6d1035b191c2e4b
snehamore23 <moresneha806@gmail.com>
2026-06-30 19:12:41 +0530

    done

---
+++ Basics/reverse.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class reverse {
+   public static void main (String args[]){
+      Scanner sc= new Scanner (System.in);
+
+      System.err.print("eneter no: ");
+      
+
+      int n= sc.nextInt();
+       
+         while (n>0){
+            int lastdigit= n%10;
+            System.out.print(lastdigit + " ");
+            n= n/10;
+         }
+    }
+    
+}
+
