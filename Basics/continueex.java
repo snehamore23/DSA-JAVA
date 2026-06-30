079b84fd8e0f506de889b32015dd714f84987c22
snehamore23 <moresneha806@gmail.com>
2026-06-30 23:20:10 +0530

    done

--- Basics/continueex.java
+++ Basics/continueex.java
@@ -1,10 +1,10 @@
 public class continueex {
     public static void main (String []args){
-        for(int i= 1; 1<=10;i ++){
-            if (i==5){
+        for(int i= 1; i<=5;i ++){
+            if (i==3){
                 continue;
             }
-            System.out.println(i)
+            System.out.println(i);
         }
 
     }
