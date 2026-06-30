9e8356be7612a938380ac2f938354966ec9cf972
snehamore23 <moresneha806@gmail.com>
2026-06-30 22:58:15 +0530

    done

---
+++ Basics/breakex.java
@@ -0,0 +1,12 @@
+public class breakex {
+    public static void main (String [] args){
+
+    for(int i = 1; i<=5; i++){
+        if (i==3){
+            break;
+        }
+        System.out.println(i);
+    }
+    Sysem.out.println("out of loop");
+    }
+}
---
+++ Basics/dowhile.java
@@ -0,0 +1,12 @@
+public class dowhile{
+    public static void main (String []args){
+
+    int counter = 1;
+    do {
+        System.out.println("hellow");
+        counter ++;
+    }while(counter<=10);
+
+    }
+}
+
