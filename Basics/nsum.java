4cbdc1b3a900393aa023aec66de678819f0eb83c
snehamore23 <moresneha806@gmail.com>
2026-06-30 18:39:20 +0530

    done

---
+++ Basics/nsum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class nsum {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+        int i = 1;
+
+        while (i <= n) {
+            sum += i;
+            i++;
+        }
+
+        System.out.println("Sum = " + sum);
+
+        sc.close();
+    }
+}
