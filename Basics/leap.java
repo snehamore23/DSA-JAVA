e88048f50a162728a0b745fdd0da3c1606119a6e
snehamore23 <moresneha806@gmail.com>
2026-06-30 13:55:15 +0530

    done

---
+++ Basics/leap.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class leap {
+    public static void main(String []args) {
+Scanner sc = new Scanner(System.in);
+
+System.out.print("enter your year:");
+int year = sc.nextInt();
+
+if (year % 4 ==0){
+   System.out.print("leap year");
+
+}else if(year % 100==0 && year % 400==0){
+    System.out.print("not a leap year");
+}else{
+    System.out.print("not a leap year");
+}
+
+}
+}
