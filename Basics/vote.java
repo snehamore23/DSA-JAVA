59c76df3a87d22d8b8d663a6089908ba862acc4c
snehamore23 <moresneha806@gmail.com>
2026-06-30 08:57:00 +0530

    done

---
+++ Basics/vote.java
@@ -0,0 +1,11 @@
+public class vote{
+    public static void main (String args[]){
+        int age =19;
+        if (age>18){
+            System.out.println("eligible for vote");
+        }
+        else{
+            System.out.println("not eligible for vote");
+        }
+    }
+}
