8ff09166e93a669d133b8336c34411acb8f46ef4
snehamore23 <moresneha806@gmail.com>
2026-07-01 15:44:03 +0530

    done

---
+++ Patterns/inverted.java
@@ -0,0 +1,11 @@
+public class inverted {
+    public static void main (String []args){
+        int n =6;
+        for (int i=1; i<=n; i++){
+            for (int j =1; j<=n-i+1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
