d32c2c3f85142f0c3834adb32cd0f3bae451ef5f
snehamore23 <moresneha806@gmail.com>
2026-07-01 15:45:02 +0530

    done

--- Patterns/star.java
+++ Patterns/star.java
@@ -2,7 +2,7 @@ public class star {
     public static void main (String []args){
         for (int line =1; line<=6; line++){
             for (int star=1; star<=line; star++){
-                System.out.print("");
+                System.out.print("*");
             }
             System.out.println();
         }
