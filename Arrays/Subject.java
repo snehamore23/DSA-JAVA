9f528942b313b9a1c801eab7b1b63f811f0493a0
snehamore23 <moresneha806@gmail.com>
2026-07-06 17:26:00 +0530

    done

---
+++ Arrays/Subject.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Subject {
+    public static void main (String args[]){
+        int marks[]= new int[100];
+        Scanner sc = new Scanner (System.in);
+
+        marks[0] = sc.nextInt();
+          marks[1] = sc.nextInt();
+            marks[2] = sc.nextInt();
+
+            System.out.println("physics:" + marks[0]);
+             System.out.println("chem:" + marks[1]);
+              System.out.println("math:" + marks[2]);
+
+              marks[2] = marks[2]+2;
+              System.out.println("math:" + marks[2]);
+
+              int percentage = (marks [0] + marks[1]+ marks[2])/3;
+              System.out.print("percentage="+ percentage + "%");
+    }
+
+
+    }
+
