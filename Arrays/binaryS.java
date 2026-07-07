a153486e27612b78a394ea79822ba24d5baf8c35
snehamore23 <moresneha806@gmail.com>
2026-07-07 13:20:45 +0530

    done with binary program

---
+++ Arrays/binaryS.java
@@ -0,0 +1,26 @@
+public class binaryS {
+    public static int binarySearch(int numbers[], int key){
+int start =0 , end= numbers.length-1;
+
+while (start<=end){
+    int mid = (start + end)/2;
+
+    //comparisions
+    if(numbers[mid]==key){
+        return mid;
+    }
+    if (numbers[mid]<key){
+        start =mid+1;
+    }else{
+        end= mid -1;
+    
+}
+}
+return -1;
+    }
+public static void main (String args[]){
+    int numbers [] = {2,4,6,8,10,12,14};
+    int key= 2;
+    System.out.println("index for key is:" + binarySearch(numbers,key));
+}
+}
